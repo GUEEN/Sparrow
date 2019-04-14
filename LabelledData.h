1d138649b250afaa5c7af6f7e44c2ed23e28ce22
GUEEN <livinskyivan@gmail.com>
2019-04-14 20:56:20 +0300

    added labeled data class

---
+++ LabelledData.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+// Training example. It consists of two fields: `feature` and `label`.
+template<class TFeature, class TLabel>
+struct LabeledData {
+
+    LabeledData(const std::vector<TFeature>& feature, TLabel label): feature(feature), label(label) {
+    }
+
+    std::vector<TFeature> feature;
+    TLabel label;
+};
+
+typedef double RawTFeature;
+typedef int TFeature;
+typedef int TLabel;
+
+typedef LabeledData<RawTFeature, TLabel> RawExample;
+typedef LabeledData<TFeature, TLabel> Example;
