b5b362762705cc7c11f55223cbe0ee53820cde21
GUEEN <livinskyivan@gmail.com>
2019-05-01 02:15:27 +0300

    added stratified data class

---
+++ StratifiedStorage.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include "StratifiedStorage.h"
+#include "SerialStorage.h"
+
+StratifiedStorage::StratifiedStorage(
+        int num_examples,
+        int feature_size,
+        const std::string& positive,
+        int num_examples_per_block,
+        const std::string& disk_buffer_filename
+        ) : positive(positive) {
+}
+
+void StratifiedStorage::init_stratified_from_file(
+    const std::string& filename,
+    int size,
+    int batch_size,
+    int feature_size,
+    Range range,
+    const std::vector<Bins>& bins
+) {
+    SerialStorage reader(
+        filename,
+        size,
+        feature_size,
+        true,
+        positive,
+        std::vector<Bins>(),
+        range
+    );
+
+    int index = 0;
+    while (index < size) {
+        std::vector<RawExample> data = reader.read_raw(batch_size);
+        for (const RawExample& rexample : data) {
+            const std::vector<RawTFeature>& raw_features = rexample.feature;
+            std::vector<TFeature> features;
+            for (int idx = 0; idx < raw_features.size(); ++idx) {
+                RawTFeature val = raw_features[idx];
+                if (range.start <= idx && idx < range.end) {
+                    features.push_back(bins[idx - range.start].get_split_index(val));
+                } else {
+                    features.push_back(0);
+                }
+            }
+            LabeledData<TFeature, TLabel> mapped_data(features, rexample.label);
+            // send mapped data
+        }
+        index += batch_size;
+    }
+
+    std::cout << "Raw data on disk has been loaded into the stratified storage " << std::endl;
+}
+
---
+++ StratifiedStorage.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "DiskBuffer.h"
+#include "Bins.h"
+
+
+class StratifiedStorage {
+public:
+
+    StratifiedStorage(
+        int num_examples,
+        int feature_size,
+        const std::string& positive,
+        int num_examples_per_block,
+        const std::string& disk_buffer_filename
+    );
+
+    void init_stratified_from_file(
+        const std::string& filename,
+        int size,
+        int batch_size,
+        int feature_size,
+        Range range,
+        const std::vector<Bins>& bins);
+
+private:
+
+    std::string positive;
+
+};
